8bf6bad1e24d696cfdd55cc5d51885be2f81a98c
Emanuel Minetti <e.minetti@posteo.de>
2017-09-22 20:52:06 +0200

    SuTS-14 Square 26 says its name is 'C9': Done!

--- test/lib/SudokuSpec.ts
+++ test/lib/SudokuSpec.ts
@@ -3,6 +3,6 @@ import {Sudoku} from "../../lib/Sudoku";
 describe('Sudoku', function () {
     it('Square names ', function () {
         let sudoku: Sudoku = new Sudoku;
-        expect(sudoku.getSquares()[26].getName()).toBe('H3');
+        expect(sudoku.getSquares()[26].getName()).toBe('I3');
     })
 });
