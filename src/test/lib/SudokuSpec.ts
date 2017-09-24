68b5d2cffd21db30c2c6c0384e9a482dfc69d666
Emanuel Minetti <e.minetti@posteo.de>
2017-09-24 22:43:39 +0200

    SuTS-10 Add tests for Sudoku.ts: Started to test a puzzle.

--- src/test/lib/SudokuSpec.ts
+++ src/test/lib/SudokuSpec.ts
@@ -2,7 +2,7 @@ import {Sudoku} from '../../lib/Sudoku';
 import {Square} from "../../lib/Square";
 import * as _ from 'lodash';
 
-describe('A Square of an empty Sudoku ', function () {
+describe('A Square of an empty Sudoku ', () => {
     let sudoku: Sudoku;
     let square: Square;
 
@@ -10,6 +10,7 @@ describe('A Square of an empty Sudoku ', function () {
         sudoku = new Sudoku;
         square = sudoku.getSquares()[26];
     });
+
     it('should report its name', () => {
         expect(square.getName()).toBe('I3');
     });
@@ -30,5 +31,28 @@ describe('A Square of an empty Sudoku ', function () {
     it('should be able to set its value', () => {
         sudoku.setValue(26, 3);
        expect(square.getValue()).toBe(3);
+    });
+
+    it('should update the candidates of its peers', () => {
+        expect(sudoku.getSquares()[square.getPeerIndices()[10]].getCandidates()).not.toContain(3);
+        expect(sudoku.getSquares()[square.getPeerIndices()[10]].getCandidates()).toContain(2);
+    });
+
+    describe('The sudoku class', () => {
+        var sudokuString =
+            '9** *** *6* \n' +
+            '**2 89* *** \n' +
+            '1** **5 2** \n' +
+            '            \n' +
+            '**1 **7 *96 \n' +
+            '*** 1*9 *** \n' +
+            '35* 4** 8** \n' +
+            '            \n' +
+            '**3 9** **2 \n' +
+            '*** *42 5** \n' +
+            '*6* *** **7 \n';
+       it('should be able to create a new Sudoku from a correct string', () => {
+           expect(Sudoku.createSudokuByString(sudokuString)).not.toBeNull();
+       });
     });
 });
