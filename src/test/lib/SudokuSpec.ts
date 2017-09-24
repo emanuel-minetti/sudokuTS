dace8bd5072706315e9aa95528f42e3ec1e81594
Emanuel Minetti <e.minetti@posteo.de>
2017-09-24 22:10:20 +0200

    SuTS-10 Add tests for Sudoku.ts: A few tests for a square in an empty sudoku.

--- src/test/lib/SudokuSpec.ts
+++ src/test/lib/SudokuSpec.ts
@@ -1,11 +1,34 @@
 import {Sudoku} from '../../lib/Sudoku';
+import {Square} from "../../lib/Square";
+import * as _ from 'lodash';
 
-describe('A Sudoku.ts ', function () {
-    it('should report right square names  for an given index ', function () {
-        let sudoku: Sudoku = new Sudoku;
-        expect(sudoku.getSquares()[26].getName()).toBe('I3');
-    })
-    it('should report a list candigates'), function () {
-        //TODO implement
-    }
+describe('A Square of an empty Sudoku ', function () {
+    let sudoku: Sudoku;
+    let square: Square;
+
+    beforeAll(() => {
+        sudoku = new Sudoku;
+        square = sudoku.getSquares()[26];
+    });
+    it('should report its name', () => {
+        expect(square.getName()).toBe('I3');
+    });
+
+    it('should report a full list of candidates', () => {
+        expect(square.getCandidates()).not.toBeNull();
+        expect(square.getCandidates()).toEqual(_.range(1,10,1));
+    });
+
+    it('should have no value set', () => {
+        expect(square.getValue()).toBeNull();
+    });
+
+    it('should report its box name', () => {
+        expect(square.getBoxName()).toBe('III');
+    });
+
+    it('should be able to set its value', () => {
+        sudoku.setValue(26, 3);
+       expect(square.getValue()).toBe(3);
+    });
 });
