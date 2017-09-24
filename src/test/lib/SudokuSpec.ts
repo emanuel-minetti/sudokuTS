bbd2ce2f09157fe4a33e54960a9ddd013d0afcda
Emanuel Minetti <e.minetti@posteo.de>
2017-09-25 00:29:50 +0200

    SuTS-10 Add tests for Sudoku.ts: 100% coverage

--- src/test/lib/SudokuSpec.ts
+++ src/test/lib/SudokuSpec.ts
@@ -38,21 +38,80 @@ describe('A Square of an empty Sudoku ', () => {
         expect(sudoku.getSquares()[square.getPeerIndices()[10]].getCandidates()).toContain(2);
     });
 
-    describe('The sudoku class', () => {
-        var sudokuString =
-            '9** *** *6* \n' +
-            '**2 89* *** \n' +
-            '1** **5 2** \n' +
-            '            \n' +
-            '**1 **7 *96 \n' +
-            '*** 1*9 *** \n' +
-            '35* 4** 8** \n' +
-            '            \n' +
-            '**3 9** **2 \n' +
-            '*** *42 5** \n' +
-            '*6* *** **7 \n';
-       it('should be able to create a new Sudoku from a correct string', () => {
-           expect(Sudoku.createSudokuByString(sudokuString)).not.toBeNull();
-       });
+    it('should report correct unit names and indices', () => {
+        expect(square.getRowName()).toEqual('3');
+        expect(square.getRowIndex()).toEqual(2);
+        expect(square.getColumnName()).toEqual('I');
+        expect(square.getColumnIndex()).toEqual(8);
+        expect(square.getBoxIndex()).toEqual(2);
     });
 });
+
+describe('The sudoku class', () => {
+    let sudoku: Sudoku;
+    let sudokuString =
+        '9** *** *6* \n' +
+        '**2 89* *** \n' +
+        '1** **5 2** \n' +
+        '            \n' +
+        '**1 **7 *96 \n' +
+        '*** 1*9 *** \n' +
+        '35* 4** 8** \n' +
+        '            \n' +
+        '**3 9** **2 \n' +
+        '*** *42 5** \n' +
+        '*6* *** **7 \n';
+
+    let falseSudokuString =
+        '9** *** *6* \n' +
+        '**2 89* *** \n' +
+        '1** **5 2** \n' +
+        '            \n' +
+        '**1 **7 *96 \n' +
+        '*** 1*9 *** \n' +
+        '35* 4** 8** \n' +
+        '            \n' +
+        '**3 9** **2 \n' +
+        '*** *42 5** \n';
+
+    it('should be able to create a new Sudoku from a correct string', () => {
+        sudoku = Sudoku.createSudokuByString(sudokuString);
+        expect(sudoku).not.toBeNull();
+    });
+
+    it('should report correct values for set values', () => {
+        expect(sudoku.getSquares()[0].getName()).toEqual('A1');
+        expect(sudoku.getSquares()[0].getValue()).toEqual(9);
+    });
+
+    it('should throw an error if a incorrect string is given', () => {
+       expect(() => Sudoku.createSudokuByString(falseSudokuString)).toThrowError('Wrong length of input!');
+    });
+
+});
+
+describe('A sudoku', () => {
+    let sudokuString =
+        '9** *** *6* \n' +
+        '**2 89* *** \n' +
+        '1** **5 2** \n' +
+        '            \n' +
+        '**1 **7 *96 \n' +
+        '*** 1*9 *** \n' +
+        '35* 4** 8** \n' +
+        '            \n' +
+        '**3 9** **2 \n' +
+        '*** *42 5** \n' +
+        '*6* *** **7 \n';
+    let sudoku = Sudoku.createSudokuByString(sudokuString);
+    let square1 = sudoku.getSquares()[0];
+
+    it('should not set an already set Value', () => {
+        expect(() => sudoku.setValue(0, 1)).toThrowError('Square already filled!');
+    })
+
+    it('should not set a value that is not a candidate', () => {
+        expect(() => sudoku.setValue(9, 2)).toThrowError();
+        expect(() => sudoku.setValue(2, 2)).toThrowError();
+    })
+})
