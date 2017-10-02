e4114111bdaacec8d21b41649084c6a650459596
Emanuel Minetti <e.minetti@posteo.de>
2017-10-02 20:43:17 +0200

    SuTS-29 Implement and test a minimal cli: started

--- src/sudoku_cli.ts
+++ src/sudoku_cli.ts
@@ -1,20 +1,9 @@
 import {SudokuGame} from "./lib/SudokuGame";
 import {Solver} from "./lib/Solver";
 
-console.log('Hello from sudoku_cli!');
-
-let sudokuString =
-    '9** *** *6* \n' +
-    '**2 89* *** \n' +
-    '1** **5 2** \n' +
-    '            \n' +
-    '**1 **7 *96 \n' +
-    '*** 1*9 *** \n' +
-    '35* 4** 8** \n' +
-    '            \n' +
-    '**3 9** **2 \n' +
-    '*** *42 5** \n' +
-    '*6* *** **7 \n';
+let sudokuStringArray = process.argv.slice(1);
+let sudokuString = sudokuStringArray.join('');
+console.log('String: ' + sudokuString)
 
 let game = new SudokuGame(sudokuString);
 let solver = new Solver(game);
