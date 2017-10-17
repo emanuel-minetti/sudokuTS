327401a1ecd56f24590eb8c87d3cff157b8fe08f
Emanuel Minetti <e.minetti@posteo.de>
2017-10-17 20:50:07 +0200

    SuTS-35 Implement the naked triple rule: Further refactored.
    Implemented a naked tuple rule and called it from naked pair rule and
    naked triple rule.

--- src/lib/solver/BasicRules.ts
+++ src/lib/solver/BasicRules.ts
@@ -25,44 +25,7 @@ export class BasicRules {
      * done according this rule
      */
     private static _npRuleFn: TRuleFunction = (sudoku) => {
-        let moves: SudokuStateChange[] = [];
-        let units = sudoku.getUnits();
-        //for each unit
-        units.forEach((unit) => {
-            //find candidates for pair
-            let pairCandidates: Square[] = RulesHelper.getUnsetSquares(unit);
-            // for these candidates find all pairs
-            let pairs = RulesHelper.getTupelsOfSquares(pairCandidates, 2);
-            //for these pairs find all naked pairs
-            pairs.forEach((pair) => {
-                let union: number[] = [];
-                pair.forEach((pairSquare) => {
-                    union = _.union(union, pairSquare.getCandidates());
-                });
-                if (union.length === 2) {
-                    //naked pair found
-                    //for this naked pair find all common units
-                    let commonUnits = RulesHelper.findCommonUnits(sudoku, pair);
-                    //for each common unit
-                    commonUnits.forEach((commonUnit) => {
-                        //for each square in this unit
-                        commonUnit.forEach((square) => {
-                            //that is not part of the pair
-                            if (_.indexOf(pair, square) === -1) {
-                                //find the values to remove
-                                let valuesToRemove = _.intersection(square.getCandidates(), union);
-                                if (valuesToRemove.length !== 0) {
-                                    let move = new SudokuStateChange(square.getIndex(), valuesToRemove,
-                                        'removed ' + valuesToRemove + ' from candidates of ' + square.getName());
-                                    moves.push(move);
-                                }
-                            }
-                        });
-                    });
-                }
-            });
-        });
-        return moves;
+        return RulesHelper.nakedTupleRule(sudoku, 2);
     }
 
     /**
@@ -75,46 +38,9 @@ export class BasicRules {
      * @returns {SudokuStateChange[]} an array of moves that could be
      * done according this rule
      */
-    private static _ntRuleFn : TRuleFunction = (sudoku => {
-        let moves: SudokuStateChange[] = [];
-        let units = sudoku.getUnits();
-        //for each unit
-         units.forEach((unit) => {
-             //find candidates for triple
-             let tripleCandidates: Square[] = RulesHelper.getUnsetSquares(unit);
-             // for these candidates find all triples
-             let triples = RulesHelper.getTupelsOfSquares(tripleCandidates, 3);
-             //for these triples find all naked triples
-             triples.forEach((triple) => {
-                let union: number[] = [];
-                triple.forEach((tripleSquare) => {
-                   union = _.union(union, tripleSquare.getCandidates());
-                });
-                if (union.length === 3) {
-                    //naked triple found
-                    //for this naked triple find all common units
-                    let commonUnits = RulesHelper.findCommonUnits(sudoku, triple);
-                    //for each common unit
-                    commonUnits.forEach((commonUnit) => {
-                       //for each square in this unit
-                       commonUnit.forEach((square) => {
-                           //that is not part of the triple
-                           if (_.indexOf(triple, square) === -1) {
-                               //find the values to remove
-                               let valuesToRemove = _.intersection(square.getCandidates(), union);
-                               if (valuesToRemove.length !== 0) {
-                                   let move = new SudokuStateChange(square.getIndex(), valuesToRemove,
-                                       'removed ' + valuesToRemove + ' from candidates of ' + square.getName());
-                                   moves.push(move);
-                               }
-                           }
-                       });
-                    });
-                }
-             });
-         });
-        return moves;
-    });
+    private static _ntRuleFn: TRuleFunction = (sudoku) => {
+        return RulesHelper.nakedTupleRule(sudoku, 3);
+    }
 
     //TODO document!
     private static _hpRuleFn: TRuleFunction = (sudoku) => {
--- src/lib/solver/RulesHelper.ts
+++ src/lib/solver/RulesHelper.ts
@@ -1,6 +1,8 @@
 import {Square} from "../game/Square";
 import * as _ from "lodash";
 import {Sudoku} from "../game/Sudoku";
+import {TRuleFunction} from "./SolverRule";
+import {SudokuStateChange} from "../game/SudokuStateChange";
 
 export class RulesHelper {
     //TODO document!
@@ -47,5 +49,47 @@ export class RulesHelper {
            intersection = _.intersection(intersection, square.getUnitIndices());
         });
         return intersection.map((unitIndex) => allUnits[unitIndex]);
+    }
+
+    //TODO document
+    static nakedTupleRule(sudoku: Sudoku, length: number): SudokuStateChange[] {
+        let moves: SudokuStateChange[] = [];
+        let units = sudoku.getUnits();
+        //for each unit
+        units.forEach((unit) => {
+            //find candidates for tuple
+            let tupleCandidates: Square[] = RulesHelper.getUnsetSquares(unit);
+            // for these candidates find all tuples
+            let tuples = RulesHelper.getTupelsOfSquares(tupleCandidates, length);
+            //for these tuples find all naked tuples
+            tuples.forEach((tuple) => {
+                let union: number[] = [];
+                tuple.forEach((tupleSquare) => {
+                    union = _.union(union, tupleSquare.getCandidates());
+                });
+                if (union.length === length) {
+                    //naked tuple found
+                    //for this naked tuple find all common units
+                    let commonUnits = RulesHelper.findCommonUnits(sudoku, tuple);
+                    //for each common unit
+                    commonUnits.forEach((commonUnit) => {
+                        //for each square in this unit
+                        commonUnit.forEach((square) => {
+                            //that is not part of the tuple
+                            if (_.indexOf(tuple, square) === -1) {
+                                //find the values to remove
+                                let valuesToRemove = _.intersection(square.getCandidates(), union);
+                                if (valuesToRemove.length !== 0) {
+                                    let move = new SudokuStateChange(square.getIndex(), valuesToRemove,
+                                        'removed ' + valuesToRemove + ' from candidates of ' + square.getName());
+                                    moves.push(move);
+                                }
+                            }
+                        });
+                    });
+                }
+            });
+        });
+        return moves;
     }
 }
