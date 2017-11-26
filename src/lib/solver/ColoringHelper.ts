8e4e2e8d6c496426aeb90eb051a42b524616f0f8
Emanuel Minetti <e.minetti@posteo.de>
2017-11-27 00:04:52 +0100

    sudokuTS-16 Implement Simple Coloring: Started implementing

--- src/lib/solver/ColoringHelper.ts
+++ src/lib/solver/ColoringHelper.ts
@@ -29,11 +29,49 @@ export class Coloring {
 //TODO implement
 export class ColoringHelper {
     static color(sudoku: Sudoku, value: number): Coloring {
+        //TODO review
         let containingSquares = sudoku.getSquares().filter(square => square.containsCandidate(value));
         let squaresToColor = containingSquares.slice();
-        while (squaresToColor.length !== 0) {
-            //TODO Start here!
-        }
+        let chains: Square[][] = [];
+        containingSquares.forEach(containingSquare => {
+            if ((squaresToColor.indexOf(containingSquare) !== -1)) {
+                let neighbours = _.intersection(sudoku.getPeersOfSquare(containingSquare), containingSquares);
+                if (neighbours.length >= 2) {
+                    let chainsWithNeighbours = chains.filter(chain => (_.intersection(chain, neighbours).length > 0));
+                    if (chainsWithNeighbours.length === 0) {
+                        chains.push([containingSquare]);
+                        _.pull(squaresToColor, containingSquare);
+                    }
+                    else if (chainsWithNeighbours.length === 1) {
+                        chainsWithNeighbours[0].push(containingSquare);
+                        _.pull(squaresToColor, containingSquare);
+                    }
+                    else {
+                        let newChain: Square[] = [containingSquare];
+                        chainsWithNeighbours.forEach(chainWithNeighbours => {
+                            _.concat(newChain, chainWithNeighbours);
+                            _.pull(chains, chainWithNeighbours);
+                        });
+                        chains.push(newChain);
+                        _.pull(squaresToColor, containingSquare);
+                    }
+                }
+                else if (neighbours.length = 1) {
+                    let chainWithNeighbor = chains.filter(chain => (chain.indexOf(neighbours[0]) !== -1));
+                    if (chainWithNeighbor.length === 1) {
+                        chainWithNeighbor.push(containingSquare);
+                    }
+                    else {
+                        chains.push([containingSquare]);
+                    }
+                    _.pull(squaresToColor, containingSquare);
+                }
+                else {
+                    _.pull(squaresToColor, containingSquare);
+                }
+            }
+        });
+        //TODO add debugging output!
 
         return new Coloring(sudoku.getSquares());
     }
