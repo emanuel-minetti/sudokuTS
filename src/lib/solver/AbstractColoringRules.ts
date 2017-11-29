a4dcbfdcd26628b309a4add6f3afe3d60fedf3dd
Emanuel Minetti <e.minetti@posteo.de>
2017-11-29 19:51:09 +0100

    sudokuTS-16 Implement Simple Coloring: Started implementing 'two colors'
    Two Colors Seen rule is working, but the moves are not yet implemented.

--- src/lib/solver/AbstractColoringRules.ts
+++ src/lib/solver/AbstractColoringRules.ts
@@ -1,6 +1,6 @@
 import {Sudoku} from "../game/Sudoku";
 import {SudokuStateChange} from "../game/SudokuStateChange";
-import {ColoringHelper} from "./ColoringHelper";
+import {ColoringHelper, Color, Coloring} from "./ColoringHelper";
 
 export class AbstractColoringRules {
 
@@ -8,9 +8,25 @@ export class AbstractColoringRules {
     //TODO comment
     //TODO implement
     static twoColorsSeen(sudoku: Sudoku) {
-        let colorings = ColoringHelper.color(sudoku, 7);
         let moves: SudokuStateChange[] = [];
-
+        Sudoku.values.forEach(value => {
+            let colorings = ColoringHelper.color(sudoku, value);
+            colorings.forEach(coloring => {
+                sudoku.getSquares().forEach(square => {
+                    if (coloring.getSquares(Color.Uncolored).indexOf(square) !== -1 && square.containsCandidate(value)) {
+                        let coloredPeers = sudoku.getPeersOfSquare(square).filter(peer => coloring.getSquares(Color.Uncolored).indexOf(peer) === -1);
+                        if (coloredPeers.length > 1) {
+                            let firstColor = Coloring.colors.filter(color => coloring.getSquares(color).indexOf(coloredPeers[0]) !== -1)[0];
+                            if (coloredPeers.reduce((hasOtherColor, peer) => (hasOtherColor || (coloring.getSquares(firstColor).indexOf(peer) === -1)), false)) {
+                                //square that sees two colors found
+                                //TODO create and push move!
+                                console.log(square.getName() + ' sees two colors!')
+                            }
+                        }
+                    }
+                });
+            });
+        });
         return moves;
     }
 }
