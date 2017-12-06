a79dca402d7b201d3df772c6b3cab49022b011b1
Emanuel Minetti <e.minetti@posteo.de>
2017-12-07 00:41:51 +0100

    sudokuTS-20 Created tests with Knuth's example.

--- src/test/lib/backtracker/DLXSpec.ts
+++ src/test/lib/backtracker/DLXSpec.ts
@@ -1,9 +1,9 @@
 import {DLX} from "../../../lib/backtracker/DLX";
 import {SimpleResultHandler} from "../../../lib/backtracker/ResultHandler";
 
-//TODO Implement real tests
 describe('A newly created DLX' , () => {
-    it('should have a representation', () => {
+    it('should be able to solve Knuth\'s Example choosing the first column', () => {
+        let resultHandler = new SimpleResultHandler();
         let dlx = new DLX(
             ['A', 'B', 'C', 'D', 'E', 'F', 'G'],
             [
@@ -15,8 +15,36 @@ describe('A newly created DLX' , () => {
                 [false, false, false, true, true, false, true]
             ],
             DLX.chooseColumnRight,
-            new SimpleResultHandler()
+            resultHandler
         );
-        expect(true).toBe(true);
+        dlx.solve();
+        expect(resultHandler.getResult()).toEqual(
+            'A D\n' +
+            'B G\n' +
+            'C E F'
+        );
+    });
+
+    it('should be able to solve Knuth\'s Example choosing the smallest column', () => {
+        let resultHandler = new SimpleResultHandler();
+        let dlx = new DLX(
+            ['A', 'B', 'C', 'D', 'E', 'F', 'G'],
+            [
+                [false, false, true, false, true, true, false],
+                [true, false, false, true, false, false, true],
+                [false, true, true, false, false, true, false],
+                [true, false, false, true, false, false, false],
+                [false, true, false, false, false, false, true],
+                [false, false, false, true, true, false, true]
+            ],
+            DLX.chooseColumnSmallest,
+            resultHandler
+        );
+        dlx.solve();
+        expect(resultHandler.getResult()).toEqual(
+            'A D\n' +
+            'E F C\n' +
+            'B G'
+        );
     });
 })
