8776404248f1da91938ce820af44bff884695803
Emanuel Minetti <e.minetti@posteo.de>
2017-12-07 19:54:19 +0100

    sudokuTS-20 Documented.

--- src/lib/backtracker/ResultHandler.ts
+++ src/lib/backtracker/ResultHandler.ts
@@ -1,10 +1,31 @@
 import {DataObject} from "./DLX";
 
+/**
+ * Must be implemented by any result handler given to {@link DLX}.
+ */
 export interface IResultHandler {
+
+    /**
+     * Implements the "print" function of the Knuth's paper.
+     *
+     * To be called by {@link DLX}
+     *
+     * @param {DataObject} root the root of {@link DLX}
+     * @param {DataObject[]} solution the solution of the {@link DLX} given to.
+     */
     processResult: (root: DataObject, solution: DataObject[]) => void;
+
+    /**
+     * Returns the result in an expected manner.
+     *
+     * @returns {any} the expected form of the solution.
+     */
     getResult: () => any;
 }
 
+/**
+ * The result handler as Knuth's paper.
+ */
 export class SimpleResultHandler implements IResultHandler {
     private resultString: string;
 
