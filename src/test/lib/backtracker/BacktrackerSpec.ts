efa5518d87836b92730840ea64fbb2d85caf5617
Emanuel Minetti <e.minetti@posteo.de>
2017-12-12 19:18:30 +0100

    sudokuTS-22 Added another test for the rows of an empty backtracker.

--- src/test/lib/backtracker/BacktrackerSpec.ts
+++ src/test/lib/backtracker/BacktrackerSpec.ts
@@ -18,11 +18,15 @@ describe('A newly created empty Backtracker', () => {
     let backtracker = new Backtracker(game);
     it('should report correct column names', () => {
         expect(backtracker.columnNames[5]).toEqual("Some number in square A6");
+        expect(backtracker.columnNames[47]).toEqual("Some number in square F3");
         expect(backtracker.columnNames[80]).toEqual("Some number in square J9");
         expect(backtracker.columnNames[81]).toEqual("1 must be present in column 1");
         expect(backtracker.columnNames[82]).toEqual("1 must be present in column 2");
+        expect(backtracker.columnNames[191]).toEqual("5 must be present in column 3");
         expect(backtracker.columnNames[90]).toEqual("1 must be present in row A");
+        expect(backtracker.columnNames[203]).toEqual("5 must be present in row F");
         expect(backtracker.columnNames[99]).toEqual("1 must be present in box I");
+        expect(backtracker.columnNames[208]).toEqual("5 must be present in box II");
         expect(backtracker.columnNames[107]).toEqual("1 must be present in box IX");
         expect(backtracker.columnNames[108]).toEqual("2 must be present in column 1");
         expect(backtracker.columnNames[323]).toEqual("9 must be present in box IX");
@@ -34,12 +38,20 @@ describe('A newly created empty Backtracker', () => {
     });
 
     it('should report correct row for "1 in A1"', () => {
-        // 1 in A1
         let oneInA1 = backtracker.columnNames.map(columnName => false);
         oneInA1[0] = true;
         oneInA1[81] = true;
         oneInA1[90] = true;
         oneInA1[99] = true;
         expect(backtracker.rows[0]).toEqual(oneInA1);
+    });
+
+    it('should report correct row for "5 in F3"', () => {
+        let fiveInF5 = backtracker.columnNames.map(columnName => false);
+        fiveInF5[47] = true;
+        fiveInF5[191] = true;
+        fiveInF5[203] = true;
+        fiveInF5[208] = true;
+        expect(backtracker.rows[0]).toEqual(fiveInF5);
     });
 });
