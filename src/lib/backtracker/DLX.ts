c9631abf69c31aa5df45039c91acf4f6ed42bb59
Emanuel Minetti <e.minetti@posteo.de>
2018-01-15 22:08:59 +0100

    sudokuTS-28 Code maintenance.

--- src/lib/backtracker/DLX.ts
+++ src/lib/backtracker/DLX.ts
@@ -1,4 +1,4 @@
-import {ColumnChooser, DataObject, ColumnObject, IResultHandler, TChooseColumnFn} from "./DLXHelpers";
+import {ColumnChooser, ColumnObject, DataObject, IResultHandler, TChooseColumnFn} from "./DLXHelpers";
 
 /**
  * A class implementing the Dancing Links implementation of Algorithm X. Also called "DLX".
