31fec55646a08808f919eab524c3b7218039c489
Emanuel Minetti <e.minetti@posteo.de>
2018-01-26 02:06:12 +0100

    sudokuTS-31 Documented.

--- src/lib/generator/Generator.ts
+++ src/lib/generator/Generator.ts
@@ -6,21 +6,38 @@ import {Sudoku} from "../game/Sudoku";
 import {Backtracker} from "../backtracker/Backtracker";
 import {ColumnChooser} from "../backtracker/DLXHelpers";
 
+/**
+ * Returns the index of a symmetry partner for the given index.
+ *
+ * @param {number} index
+ * @returns {number}
+ */
 export type SymmetryFunction = (index: number) =>  number;
 
-//TODO document!
+/**
+ * A class exporting {@code SymmetryFunction}s to be used by the {@code Generator}.
+ */
 export class Symmetry  {
 
+    /**
+     * The central symmetry. {@see SymmetryFunction}
+     */
     static central: SymmetryFunction = (index: number) =>  {
         return 80 - index;
     }
 
+    /**
+     * The diagonal symmetry along the main diagonal of a mtrix. {@see SymmetryFunction}
+     */
     static diagonal = (index: number) => {
         let row = Math.floor(index / 9);
         let column = index % 9;
         return column * 9 + row;
     }
 
+    /**
+     * No symmetry. {@see SymmetryFunction}
+     */
     static noSymmetry = (index: number) => {
         return Generator.getRandomIndex();
     }
