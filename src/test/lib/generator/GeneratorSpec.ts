e664bfc9e5a9f8e135bafc406cb2cf753a674bc0
Emanuel Minetti <e.minetti@posteo.de>
2018-01-27 22:05:22 +0100

    sudokuTS-31 Added TODO

--- src/test/lib/generator/GeneratorSpec.ts
+++ src/test/lib/generator/GeneratorSpec.ts
@@ -1,5 +1,6 @@
 import {Generator, Symmetry} from "../../../lib/generator/Generator";
 
+//TODO add and refine tests
 describe('A Generator', () => {
     it('should return a puzzle with central symmetry', function () {
         let puzzle = Generator.generate(0, 200, 1, Symmetry.central);
