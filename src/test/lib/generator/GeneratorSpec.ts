14f01cfcf0d1850debb8f68a27bdf2a4c753f793
Emanuel Minetti <e.minetti@posteo.de>
2018-01-29 21:44:21 +0100

    sudokuTS-31 Added tests.

--- src/test/lib/generator/GeneratorSpec.ts
+++ src/test/lib/generator/GeneratorSpec.ts
@@ -1,9 +1,20 @@
 import {Generator, Symmetry} from "../../../lib/generator/Generator";
 
-//TODO add and refine tests
 describe('A Generator', () => {
     it('should return a puzzle with central symmetry', function () {
         let puzzle = Generator.generate(0, 200, 1, Symmetry.central);
         expect(puzzle).not.toBeNull();
+    });
+    it('should return a puzzle with diagonal symmetry', function () {
+        let puzzle = Generator.generate(0, 200, 1, Symmetry.diagonal);
+        expect(puzzle).not.toBeNull();
+    });
+    it('should return a puzzle with no symmetry', function () {
+        let puzzle = Generator.generate(0, 200, 1, Symmetry.noSymmetry);
+        expect(puzzle).not.toBeNull();
+    });
+    it('should return null with too high a rating', function () {
+        let puzzle = Generator.generate(200, 201, 1, Symmetry.central);
+        expect(puzzle).toBeNull();
     });
 });
